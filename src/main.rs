1b760452a62a42749ab16dd858aea4a8f9f4d196
thehouseplant <sean.collins@outlook.com>
2019-03-10 15:23:18 -0700

    Formatting tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+
+use actix_web::{
+    server, App, HttpRequest
+};
 
 fn index(_req: &HttpRequest) -> &'static str {
     "Hello world!"
