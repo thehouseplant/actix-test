e789a40ac98c5e7ad6bbf93bf942b08bf5086a68
thehouseplant <sean.collins@outlook.com>
2019-03-10 13:38:28 -0700

    Add Actix server start to load index function

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,10 @@ use actix_web::{server, App, HttpRequest};
 fn index(_req: &HttpRequest) -> &'static str {
     "Hello world!"
 }
+
+fun main() {
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
+}
